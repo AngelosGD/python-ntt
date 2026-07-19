c96dfe45d6911516db9fc0dad8998dad252aba25
AngelosGD <angelde9919@gmail.com>
2026-07-18 21:53:19 -0600

    metodos lisstas -copy -append -clear -count

--- basico/variables.py
+++ basico/variables.py
@@ -1,4 +1,4 @@
-def sumar(n1, n2):
+""" [def sumar(n1, n2):
     resultado = n1 + n2
     print(resultado)
     
@@ -33,3 +33,23 @@ def siesmayor():
         
 siesadmin()
 siesmayor()
+]
+ """
+# listas
+
+frutas = ["apple", "manzana","bamama","uvas", "arandano", "manzana", "apple"]
+
+# print(frutas[0]) # ? primer elemento
+# print(frutas[-1]) # ? el -1 hace referencia al ultimo elemento
+# print(frutas[0:4]) # ? slice es decir recore desde el emento 0 hasta el 4, el 4 no se muestra es donde para, se muestra 1 antes
+
+
+# #? metodos de litas
+# frutas.append('xd')
+# print(frutas)
+
+# #? copia los elemento de una lista y se puede encasillar en una nueva con ese copy
+# frutas2 = frutas.copy()
+# frutas2.append('pepino')
+# print(frutas2)
+print(frutas.count('apple'))
