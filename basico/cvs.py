b286b45092417d397f1ef7d697e20683c6d53493
AngelosGD <angelde9919@gmail.com>
2026-07-27 21:56:56 -0600

    abrir archivos principios basicos -r, w y a -aprendi a imprmir el archivo y a cambiar su contendio xd sigue CVS

---
+++ basico/cvs.py
@@ -0,0 +1,25 @@
+
+#? con el with y la sentencia open se habre el archivo y le ponemos un alias:
+
+#todo tiene 3 propiedades
+# #* r -lectura w-escritura a agregar al final sin borrar lo anterior (appenda)
+# with open ("archivo.txt", "r") as f:
+#     contenido = f.read()
+#     print(contenido)
+
+# #? asi se escribe todo el contendio del archivo linea por linea
+# with open ("archivo.txt", "r") as f:
+#     for linea in f:
+#         print(linea)
+
+# #? asi se escribe en el archjvo
+# #todo como se uso el permiso "w" se ssobrecribe todo y se borra xd
+# with open("archivo.txt", "w") as f:
+#     f.write("hola \n")
+#     f.write("angel \n")
+
+# with open ("archivo.txt", "r") as f:
+#     for linea in f:
+#         print(linea)
+
+    
